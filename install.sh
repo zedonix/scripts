f88ab751ce89de73a727bfd0b62e3e1a711f7bcb
Zed <197267174+zedonix@users.noreply.github.com>
2025-04-15 21:54:37 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ mount "${disk}1" /mnt/boot
 # Base Installation
 install_pkgs=(
     base base-devel linux linux-headers linux-firmware libxkbcommon-x11 sudo man-db man-pages snapper snap-pac btrfs-progs
-    openssh gzip ncdu htop fastfetch bat eza fzf git ripgrep ripgrep-all sqlite ntfs-3g exfat-utils mtools dosfstools 
+    openssh ncdu htop fastfetch bat eza fzf git ripgrep ripgrep-all sqlite ntfs-3g exfat-utils mtools dosfstools 
     networkmanager ufw newsboat pipewire wireplumber pipewire-pulse pipewire-alsa pipewire-audio pipewire-jack mpv easyeffects
     xorg-xwayland xdg-desktop-portal-wlr xdg-desktop-portal-gtk sway swaybg swayimg swaylock swayidle foot mako fuzzel 
     papirus-icon-theme noto-fonts noto-fonts-cjk noto-fonts-emoji clang lua python go ttc-iosevka ttf-iosevkaterm-nerd gnome-themes-extra 
