6995494026b6006f6c29745a8d3548c4930b0176
Your Name <you@example.com>
2025-05-06 18:04:23 +0530

    paru

--- aur.sh
+++ aur.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
-aur_pkgs=(
-    sway-audio-idle-inhibit-git
-    swayosd-git
-    github-cli-git
-)
-
 aur_dir="$HOME/.aur"
 mkdir -p "$aur_dir"
 cd "$aur_dir"
 
-for pkg in "${aur_pkgs[@]}"; do
-    read -p "Install $pkg? [Y/n] " -r
-    if [[ $REPLY =~ ^[Yy]?$ ]]; then
-        git clone "https://aur.archlinux.org/$pkg.git"
-        cd "$pkg"
-        less PKGBUILD
-        read -p "Build $pkg? [Y/n] " -r
-        if [[ $REPLY =~ ^[Yy]?$ ]]; then
-            makepkg -si --noconfirm --needed
-        fi
-        cd ..
-    fi
-done
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+paru -S ungoogled-chromium-bin chromium-extension-return-youtube-dislike chromium-extension-ublock-origin sway-audio-idle-inhibit
