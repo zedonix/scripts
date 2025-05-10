8a023c7cb61a1fc460bc7011b5993c1439df2649
Your Name <you@example.com>
2025-05-10 12:20:35 +0530

    a

--- run.sh
+++ run.sh
@@ -39,11 +39,12 @@ gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 
 # Mime setup
-find /usr/share/applications ~/.local/share/applications -iname '*.desktop' -print0 | while IFS= read -r -d $'\0' d; do
-  for m in $(grep MimeType "$d" | cut -d= -f2 | tr ";" " "); do
-    echo xdg-mime default "'$d'" "'$m'"
-  done
-done
+# find /usr/share/applications ~/.local/share/applications -iname '*.desktop' -print0 | while IFS= read -r -d $'\0' d; do
+#   for m in $(grep MimeType "$d" | cut -d= -f2 | tr ";" " "); do
+#     echo xdg-mime default "'$d'" "'$m'"
+#   done
+# done
+for type in pdf x-pdf fdf xdp xfdf pdx; do xdg-mime default org.pwmt.zathura.desktop application/$type; done
 
 # Snapper setup
 sudo snapper -c root create-config /
