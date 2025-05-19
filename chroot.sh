88d4ae0596de3dbd1a96a6fecb535febe07f5fca
Zed <zedonix@proton.me>
2025-05-19 22:09:09 +0530

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -37,7 +37,7 @@ echo "Defaults timestamp_timeout=-1" > /etc/sudoers.d/timestamp
 chmod 440 /etc/sudoers.d/wheel /etc/sudoers.d/timestamp
 
 # Bootloader
-grub-install --target=x86_64-efi --efi-directory=/efi --boot-directory=/boot --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 sed -i 's/^#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
