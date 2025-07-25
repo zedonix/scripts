9a4315892dbaff69110606383d3108a29bedd72b
zedonarch <zedonix@proton.me>
2025-07-25 10:13:15 +0530

    no need of it but still here cuz why not;

--- sway.sh
+++ sway.sh
@@ -1,10 +1,29 @@
 #!/bin/sh
 
-export PATH="$PATH:$HOME/Documents/scripts/bin"
+export PATH="$PATH:$HOME/.local/bin"
 
 export XDG_CURRENT_DESKTOP=sway
 export XDG_SESSION_DESKTOP=sway
+export XDG_SESSION_TYPE=wayland
 export QT_QPA_PLATFORM=wayland
+export QT_QPA_PLATFORMTHEME=qt6ct
 export QT_STYLE_OVERRIDE=kvantum
+
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_STATE_HOME="$HOME/.local/state"
+
+export CARGO_HOME="$XDG_DATA_HOME"/cargo
+export GOPATH="$XDG_DATA_HOME"/go
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
+
+export TERMINAL=foot
+export COLORTERM=truecolor
+
+export EDITOR=nvim
+export VISUAL=nvim
+export SYSTEMD_EDITOR=nvim
+export MANPAGER="nvim +Man!"
 
 exec /usr/bin/sway
