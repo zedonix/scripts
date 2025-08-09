d4df189ccca62182709e187bb02e2dee1f5b97cd
zedonarch <zedonix@proton.me>
2025-08-09 20:04:29 +0530

    betterfox.sh

---
+++ bin/betterfox.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+trash "$HOME/Documents/user.js" &> /dev/null
+trash "$HOME/Documents/userMY.js" &> /dev/null
+awk '
+BEGIN {skip=0}
+{
+    if ($0 ~ /START: MY OVERRIDES/) { skip=1; print; next }
+    if (skip && $0 ~ /SECTION: SMOOTHFOX/) { skip=0; print; next }
+    if (!skip) print
+}
+' "$HOME/Documents/default/dotfiles/user.js" >"$HOME/Downloads/userMY.js"
+curl -Lo "$HOME/Downloads/user.js" https://raw.githubusercontent.com/yokoffing/Betterfox/main/user.js
+delta --side-by-side "$HOME/Downloads/userMy.js" "$HOME/Downloads/user.js"
