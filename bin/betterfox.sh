30dd8d45caa6ab4954159a7b68fe3151558a6f4a
zedonarch <zedonix@proton.me>
2025-08-29 07:57:02 +0530

    bookmark.sh and betterfox is merged

--- bin/betterfox.sh
+++ bin/betterfox.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
+
+trash ~/Documents/projects/default/dotfiles/book* &>/dev/null
 trash "$HOME/Downloads/user.js" &>/dev/null
 trash "$HOME/Downloads/userMY.js" &>/dev/null
+
+latest=$(find ~/.mozilla/firefox/*.default-release/bookmarkbackups -type f -name '*.jsonlz4' -printf '%T@ %p\n' |
+  sort -nr |
+  awk 'NR==1 {print substr($0, index($0,$2))}')
+cp -f "$latest" ~/Documents/projects/default/dotfiles/
+
 awk '
 BEGIN {skip=0}
 {
--- bin/bookmark.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-trash ~/Documents/projects/default/dotfiles/book*
-latest=$(find ~/.mozilla/firefox/*.default-release/bookmarkbackups -type f -name '*.jsonlz4' -printf '%T@ %p\n' |
-  sort -nr |
-  awk 'NR==1 {print substr($0, index($0,$2))}')
-cp -f "$latest" ~/Documents/projects/default/dotfiles/
