b1218fb6d587a00ffd163213291aaa505e31a0ed
zedonarch <zedonix@proton.me>
2025-10-03 20:38:19 +0530

    projects -> personal

--- bin/betterfox.sh
+++ bin/betterfox.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-trash ~/Documents/projects/default/dotfiles/book* &>/dev/null
+trash ~/Documents/personal/default/dotfiles/book* &>/dev/null
 trash "$HOME/Downloads/user.js" &>/dev/null
 trash "$HOME/Downloads/userMY.js" &>/dev/null
 
 latest=$(find ~/.mozilla/firefox/*.default-release/bookmarkbackups -type f -name '*.jsonlz4' -printf '%T@ %p\n' |
   sort -nr |
   awk 'NR==1 {print substr($0, index($0,$2))}')
-cp -f "$latest" ~/Documents/projects/default/dotfiles/
+cp -f "$latest" ~/Documents/personal/default/dotfiles/
 
 awk '
 BEGIN {skip=0}
@@ -16,6 +16,6 @@ BEGIN {skip=0}
     if (skip && $0 ~ /SECTION: SMOOTHFOX/) { skip=0; print; next }
     if (!skip) print
 }
-' "$HOME/Documents/projects/default/dotfiles/user.js" >"$HOME/Downloads/userMY.js"
+' "$HOME/Documents/personal/default/dotfiles/user.js" >"$HOME/Downloads/userMY.js"
 curl -Lo "$HOME/Downloads/user.js" https://raw.githubusercontent.com/yokoffing/Betterfox/main/user.js
 delta --side-by-side "$HOME/Downloads/userMY.js" "$HOME/Downloads/user.js"
