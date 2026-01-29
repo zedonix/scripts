6ad14cbe2014f3a9a9eb4830a17bbf1c81f4df6e
zedonarch <zedonix@proton.me>
2026-01-29 09:49:27 +0530

    added width 100 to fuzzed in clipvault.sh and formatting

--- bin/clipvault.sh
+++ bin/clipvault.sh
@@ -10,15 +10,15 @@ thumbnails_dir="${XDG_CACHE_HOME:-$HOME/.cache}/clipvault/thumbs"
 
 # Delete thumbnails that are no longer in the DB
 find "$thumbnails_dir" -type f | while IFS= read -r thumbnail; do
-    id=$(basename "${thumbnail%.*}")
-    if ! grep -q "^${id}\s\[\[ binary data" <<< "$list"; then
-        rm "$thumbnail"
-    fi
+  id=$(basename "${thumbnail%.*}")
+  if ! grep -q "^${id}\s\[\[ binary data" <<<"$list"; then
+    rm "$thumbnail"
+  fi
 done
 
 # Generates thumbnails (for matched image formats) for entries which don't already have one in the
 # thumbnails directory, and returns entries ready to be displayed by `fuzzel`.
-read -r -d '' prog << EOF
+read -r -d '' prog <<EOF
 /^[0-9]+\s<meta http-equiv=/ { next }
 match(\$0, /^([0-9]+)\s\[\[\sbinary.*(jpg|jpeg|png|bmp|webp|tif|gif)/, grp) {
     image = grp[1]"."grp[2]
@@ -29,22 +29,22 @@ match(\$0, /^([0-9]+)\s\[\[\sbinary.*(jpg|jpeg|png|bmp|webp|tif|gif)/, grp) {
 1
 EOF
 
-choice=$(echo "$list" | gawk "$prog" | fuzzel -d --placeholder "Clipboard" --counter --no-sort --with-nth 2)
+choice=$(echo "$list" | gawk "$prog" | fuzzel -d --width=100 --placeholder "Clipboard" --counter --no-sort --with-nth 2)
 exit_code=$?
 
 # Custom keybinds (configured in your `fuzzel.ini`) used below for different actions.
 # Delete all entries with `custom-0` (default: ALT+0)
 if [ "$exit_code" -eq 19 ]; then
-    confirmation=$(echo -e "N\ny" | fuzzel -d --placeholder "Delete history?" --lines 2)
-    [ "$confirmation" == "y" ] && rm -rf "$thumbnails_dir" && clipvault clear
+  confirmation=$(echo -e "N\ny" | fuzzel -d --placeholder "Delete history?" --lines 2)
+  [ "$confirmation" == "y" ] && rm -rf "$thumbnails_dir" && clipvault clear
 # Delete selected entry with `custom-1` (default: ALT+1)
 elif [ "$exit_code" -eq 10 ]; then
-    if [ "$choice" != "" ]; then
-        id=$(echo "$choice" | cut -f1)
-        clipvault delete "$id"
-        find "$thumbnails_dir" -name "${id}.*" -delete
-    fi
+  if [ "$choice" != "" ]; then
+    id=$(echo "$choice" | cut -f1)
+    clipvault delete "$id"
+    find "$thumbnails_dir" -name "${id}.*" -delete
+  fi
 # Default case, copy entry to clipboard
 else
-    [ "$choice" = "" ] || echo "$choice" | clipvault get | wl-copy
+  [ "$choice" = "" ] || echo "$choice" | clipvault get | wl-copy
 fi
